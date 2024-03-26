e3101ffcba74a4ab1ea568222bf2f9730b1bae10
N0E-P <pecci.noe@gmail.com>
2024-03-26 19:50:09 +0100

    red

--- components/en/1-Main.js
+++ components/en/1-Main.js
@@ -7,13 +7,13 @@ export default function Main() {
 			<div className="w-full mt-5 sm:mt-15 md:mt-10 text-center h-screen max-w-[1240px] mx-auto p-2 justify-center">
 				<h1 className="text-gray-800">Welcome to</h1>
 				<Link href={"/"}>
-					<h1 className="py-6 text-blue-700 underline">Not-A-Scam.com</h1>
+					<h1 className="py-6 text-red-500 underline">Not-A-Scam.com</h1>
 				</Link>
 				<div className="py-5">
 					<p className="py-2 text-gray-700 text-center text-lg md:text-xl lg:text-2xl ">
 						"I've made my first trillion thanks to not-a-scam.com"
 					</p>
-					<p className=" px-2 text-blue-700 text-center text-lg md:text-xl lg:text-2xl  ">
+					<p className=" px-2 text-red-500 text-center text-lg md:text-xl lg:text-2xl  ">
 						- Elon Musk
 					</p>
 					<div className="flex justify-center">
@@ -48,7 +48,7 @@ export default function Main() {
 				<div className="flex justify-center mt-12">
 					<Link href="/#presentation">
 						<div className="animate-bounce rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer">
-							<HiChevronDoubleDown className="text-blue-700" size={36} />
+							<HiChevronDoubleDown className="text-red-500" size={36} />
 						</div>
 					</Link>
 				</div>
