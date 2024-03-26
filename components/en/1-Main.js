14e770fb0a1e958a55142382ad2ffe6276886576
N0E-P <pecci.noe@gmail.com>
2024-03-26 14:13:08 +0100

    presentation

--- components/en/1-Main.js
+++ components/en/1-Main.js
@@ -4,85 +4,46 @@ import { AiOutlineMail } from "react-icons/ai";
 import Link from "next/link";
 
 export default function Main() {
-	const [changingText, changeText] = useState(
-		"The reason I became a developer while I was in sports college."
-	);
-	var words = [
-		"The projects I designed, with all the problems I went through.",
-		"My adventures in cryptos, and the stupid mistakes I made.",
-		"All my programming skills, and the tools I learned to use.",
-		"The reason I became a developer while I was in sports college.",
-	];
-	var number = 0;
-
-	useEffect(() => {
-		const interval = setInterval(() => {
-			changeText(words[number]);
-			if (number < 3) {
-				number = number + 1;
-			} else {
-				number = 0;
-			}
-		}, 5000);
-
-		return () => clearInterval(interval);
-	}, []);
-
 	return (
 		<div>
-			<div className="flex max-w-[1500px] m-auto justify-between items-center h-full">
-				<Link href="/">
-					<img
-						className=" bg-blue-100 rounded-full m-2 mt-4 sm:m-5 shadow-lg shadow-gray-400 p-1 hover:scale-105 ease-in duration-300"
-						src={"me/profile-photo.jpg"}
-						alt="Noé Pecci"
-						width="200"
-						height="200"
-					/>
-				</Link>
-				<div className="sm:flex ">
-					<Link href="/#contact">
-						<div className="bg-blue-100 m-3 justify-center hidden sm:flex rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
-							<AiOutlineMail size={50} className="pl-2" />
-							<p className="pl-3 py-2 font-bold text-2xl hidden lg:flex">
-								Contact me!
-							</p>
-							<span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500">
-								<span className="relative animate-ping inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
-							</span>
-						</div>
-					</Link>
-					<Link href="/fr">
-						<img
-							className="m-4 rounded-xl hover:scale-110 ease-in duration-300 cursor-pointer ml-5"
-							src={"flags/fr.png"}
-							alt="Français"
-							width="100"
-							height="75"
-						/>
-					</Link>
-					<Link href="/">
-						<img
-							className="m-4 sm:mr-8 rounded-xl hover:scale-110 ease-in duration-300 cursor-pointer "
-							src={"flags/en.png"}
-							alt="English"
-							width="110"
-							height="75"
-						/>
-					</Link>
-				</div>
-			</div>
 			<div className="w-full mt-5 sm:mt-15 md:mt-10 text-center h-screen max-w-[1240px] mx-auto p-2 justify-center">
-				<h1 className="text-gray-800">Hello, I'm Noé!</h1>
-				<h1 className="py-6 text-blue-700">A French Developer in</h1>
-				<h1 className="text-gray-800">Javascript & Solidity</h1>
-				<div className="py-7">
+				<h1 className="text-gray-800">Welcome to</h1>
+				<h1 className="py-6 text-blue-700">Not-A-Scam.com</h1>
+				<div className="py-5">
 					<p className="py-2 text-gray-700 text-center text-lg md:text-xl lg:text-2xl ">
-						I made this portfolio to present:
+						"I've made my first trillion thanks to not-a-scam.com"
 					</p>
 					<p className=" px-2 text-blue-700 text-center text-lg md:text-xl lg:text-2xl  ">
-						{changingText}
+						- Elon Musk
 					</p>
+					<div className="flex justify-center">
+						<img src={"stars.png"} width="250px" height="351px" alt="/" />
+					</div>
+				</div>
+				<div className="flex justify-center">
+					<div className="grid grid-cols-3 gap-10">
+						<img
+							className=" bg-blue-100 rounded-full m-2 mt-4 sm:m-5 shadow-lg shadow-gray-400 p-1 hover:scale-105 ease-in duration-300"
+							src={"me/profile-photo.jpg"}
+							alt=""
+							width="200"
+							height="200"
+						/>
+						<img
+							className=" bg-blue-100 rounded-full m-2 mt-4 sm:m-5 shadow-lg shadow-gray-400 p-1 hover:scale-105 ease-in duration-300"
+							src={"me/profile-photo.jpg"}
+							alt=""
+							width="200"
+							height="200"
+						/>
+						<img
+							className=" bg-blue-100 rounded-full m-2 mt-4 sm:m-5 shadow-lg shadow-gray-400 p-1 hover:scale-105 ease-in duration-300"
+							src={"me/profile-photo.jpg"}
+							alt=""
+							width="200"
+							height="200"
+						/>
+					</div>
 				</div>
 				<div className="flex justify-center mt-12">
 					<Link href="/#presentation">
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Main from "../components/en/1-Main";
 import Presentation from "../components/en/2-Presentation";
-import Tools from "../components/en/3-Tools";
 import Contact from "../components/en/4-Contact";
 import Project1 from "../components/en/Project1";
 import Project2 from "../components/en/Project2";
@@ -15,11 +14,9 @@ export default function Home() {
 	return (
 		<div>
 			<Head>
-				<title>Noé's Portfolio</title>
+				<title>Not A Scam</title>
 				<link rel="icon" href="/favicon.ico" />
-				<meta property="og:title" content="Noé's Portfolio" /> 
-				<meta property="og:description" content="Javascript & Solidity Developer" />
-				<meta property="og:image" content={"https://noe-pecci.com/me/profile-photo.jpg"}/>
+				<meta property="og:title" content="Not A Scam" />
 			</Head>
 			<Main />
 			<Presentation />
@@ -30,7 +27,6 @@ export default function Home() {
 			<Project5 />
 			<Project6 />
 			<Project7 />
-			<Tools />
 			<Contact />
 		</div>
 	);
